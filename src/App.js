e663e98945f0d769e1dc24dad4c5c0853e6fd203
baromeg <baromeg@gmail.com>
2021-03-03 12:11:58 +0000

    Refactored using beforeAll() on repeted code

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import React, { Component } from "react"
+import React, { Component, useState } from "react"
 import PersonList from "./PersonList"
 
 class App extends Component {
+  // const [state, setstate] = useState({ })
+  state = { people: []}
   render() {
-    return(
-    <div className='App'>
-      <PersonList />
-    </div>
-  )
+    return (
+        <div className='App'>
+          <PersonList />
+        </div>
+      )
   }
-  
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -4,20 +4,27 @@ import PersonList from "./PersonList"
 import { shallow } from "enzyme"
 
 describe("App", () => {
-  it("Renders without crashing", () => {
-    const appWrapper = shallow(<App />)
+  let appWrapper
+  beforeAll(() => {
+    appWrapper = shallow(<App />)
   })
 
   it("Renders a person list", () => {
-    const appWrapper = shallow(<App />)
     const personList = appWrapper.find(PersonList)
 
     expect(personList).toHaveLength(1)
   })
 
-  it("Renders the state of app ", () => {
-    const appWrapper = shallow(<App />)
+  it("Has state", () => {
     const appState = appWrapper.state()
+
+    expect(appState).not.toBeNull()
+  })
+
+  it("Has a people property on state", () => {
+    const appState = appWrapper.state()
+
+    expect(appState.people).toBeDefined()
   })
   
 })
